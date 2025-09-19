33d6a54791c24818b167b193a21490c7190f3131
sujitha1706 <sujithaaparna1706@gmail.com>
2025-09-19 15:54:13 +0530

    First commit - added bank management system project

---
+++ src/bank/management/system/sql.sql
@@ -0,0 +1,3 @@
+create database banksystem;
+use banksystem;
+create table signup(form_no varchar(30), name varchar(30), father_name varchar(30), DOB varchar(30), gender varchar(30), email varchar(60), martial_status varchar(30), address varchar(60), city varchar(30), pincode varchar(30), state varchar(50));
